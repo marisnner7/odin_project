353fc9dc4dda0e0a5a3291127c831bde5af98c54
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-09-04 14:11:32 -0300

    rubocop lint fixes

--- cease_cipher/lib/cease_cipher.rb
+++ cease_cipher/lib/cease_cipher.rb
@@ -1,21 +1,21 @@
-puts "What would you like to encrypt?"
+puts 'What would you like to encrypt?'
 message = gets.chomp
 # Collect an integer from a user
-puts "How many places do you want to shift the cipher?"
+puts 'How many places do you want to shift the cipher?'
 code = gets.chomp.to_i
 
 def caesar(message, code)
-  coded  = message.bytes.map do|digit|
-    if digit.between?(97, 122) #97 --122 Ascii lowcase notations for characteres
+  coded = message.bytes.map do |digit|
+    if digit.between?(97, 122) # 97 --122 Ascii lowcase notations for characteres
       (digit - 97 + code) % 26 + 97
     elsif digit.between?(65, 90) # uppercase notation
       (digit - 65 + code) % 26 + 65
 
     else
       digit
-    end  
+    end
   end
   coded.map(&:chr).join
 end
 
-puts caesar(message,code)
+puts caesar(message, code)
